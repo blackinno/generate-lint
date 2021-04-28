0949a8ff423c8d2b1e090259a4189725e88a8b7f
blackinno <ruthamnong.thotsaphon@gmail.com>
2021-04-28 16:34:08 +0700

    set format log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // #!/usr/bin/env node
+const { exec } = require('child_process')
+const { writeFile, readdir, readFile } = require('fs').promises
+const chalk = require('chalk')
 const inquirer = require('inquirer')
 const path = require('path')
-const { writeFile, readdir, readFile } = require('fs').promises
+
+const { install } = require('./install-command')
 
 const configLanguageTypes = {}
 const fileDetails = {}
@@ -19,10 +23,11 @@ async function bootstrap() {
       type: 'list',
       message: "Pick the NodeJS language framework you're using:",
       name: 'language',
+      default: 'javascript',
       choices: Object.keys(configLanguageTypes)
     }
   ])
-
+  const installCommand = install[language]
   const files = await readdir(path.join(configFolderPath, language)).catch(console.log)
 
   for (let file of files) {
@@ -36,6 +41,15 @@ async function bootstrap() {
     const object = JSON.stringify(fileDetails[key], null, 2)
     await writeFile(path.join(process.cwd(), `.${key}`), object)
   }
+
+  console.log(`${chalk.blue.bold('[INFO]')}: Install used packages...`)
+
+  const installing = exec(installCommand)
+
+  installing.on('close', () => {
+    const text = `${chalk.green.bold('[SUCCESS]')}: Install package success`
+    console.log(text)
+  })
 }
 
 bootstrap()
