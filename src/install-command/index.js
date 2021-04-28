f6df767139dbf79f59a419ee95665528154b1641
blackinno <ruthamnong.thotsaphon@gmail.com>
2021-04-28 16:03:03 +0700

    set install dependencies command

---
+++ src/install-command/index.js
@@ -0,0 +1,6 @@
+module.exports.install = {
+  javascript:
+    'npm install --save-dev eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-promise eslint-plugin-react eslint-plugin-react-hooks prettier prettier-eslint-cli',
+  typescript:
+    'npm install --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-deprecation eslint-plugin-import eslint-plugin-prettier eslint-plugin-unused-imports prettier prettier-plugin-organize-imports pretty-quick'
+}
